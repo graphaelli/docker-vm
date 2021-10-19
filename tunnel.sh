541ae488dc04344678e297ff3f97332750762169
Gil Raphaelli <g@raphaelli.com>
2021-10-18 22:11:19 -0400

    just cd

--- tunnel.sh
+++ tunnel.sh
@@ -11,9 +11,12 @@ SSH_CONFIG=$D/$1.ssh-config
 SSH_CONTROL=$D/.$1-ssh
 
 mkdir -p $D
+pushd ${SCRIPTPATH}
 vagrant ssh-config > ${SSH_CONFIG}
+popd
 cat >> ${SSH_CONFIG} <<EOF
   ExitOnForwardFailure yes
   LocalForward $1 127.0.0.1:$1
 EOF
+echo tunneling $1
 ssh -M -N -f -F ${SSH_CONFIG} -S ${SSH_CONTROL} docker-vm
